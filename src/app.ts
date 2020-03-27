031254e7c4ce95fb73a8f8b21c1846fbb7810b87
Adam Kovář <kovarada@fel.cvut.cz>
2020-03-27 23:52:29 +0100

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fetchMarkers from './osm-api-broker'
 
+// Print shops of type 'mall' and 'supermarket' bounded by 49.99,14.94,50.27,15.05
 fetchMarkers('49.99,14.94,50.27,15.05',  ['shop', 'mall'], ['shop', 'supermarket']).
 then((markers:any)=>console.log(markers))
 
